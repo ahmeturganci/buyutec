80bc6a287737ff96b524964474cad2254d1f7b58
ahmeturganci <aahmet.272@gmail.com>
2016-12-24 13:02:16 +0300

    süreçle ilgili sınıf eklendi.

---
+++ Buyutec/Models/DataViewModel/Surec.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Buyutec.Models.DataModel;
+
+namespace Buyutec.Models.DataViewModel
+{
+    public class Surec
+    {
+        public int surecId { get; set; }
+        public string surecAdi { get; set; }
+        public Nullable<System.DateTime> baslangicTarihi { get; set; }
+        public Nullable<System.DateTime> bitisTarihi { get; set; }
+        public Nullable<int> bitirmeOrani { get; set; }
+        public Nullable<int> durumId { get; set; }
+        public Nullable<int> oncelikId { get; set; }
+        public Nullable<int> projeId { get; set; }
+        public string aciklama { get; set; }
+
+        public static Surec MapData(tblSurec s)
+        {
+            Surec surec = new Surec()
+            {
+                surecAdi=s.surecAdi
+            };
+            return surec;
+        }
+
+        public static tblSurec MapData(Surec s)
+        {
+            tblSurec surec = new tblSurec()
+            {
+                surecAdi = s.surecAdi
+            };
+            return surec;
+        }
+        public static List<Surec> MapData(List<tblSurec> SurecList)
+        {
+            List<Surec> liste = new List<Surec>();
+
+            foreach (var b in SurecList)
+            {
+                liste.Add(MapData(b));
+            }
+            return liste;
+        }
+        public static List<tblSurec> MapData(List<Surec> SurecList)
+        {
+            List<tblSurec> liste = new List<tblSurec>();
+
+            foreach (var b in SurecList)
+            {
+                liste.Add(MapData(b));
+            }
+            return liste;
+        }
+    }
+}
